9f732c95405d57c96dad173b3ffd8021ea346b61
Zanidean <`AZanidean@mhc.ab.ca`>
2019-08-09 15:08:12 -0600

    add parr

--- R/auto_mm.R
+++ R/auto_mm.R
@@ -11,17 +11,37 @@
 #'@export extract_model_metric
 #'@export model_maker
 
-auto_mm = function(dataframe, model, split_vars, min_sample_size = 30, permutations = 1){
+auto_mm = function(dataframe,
+                   model,
+                   split_vars,
+                   min_sample_size = 30,
+                   permutations = 1,
+                   parallelize = F){
+
+  if(parallelize == T){
+    require(furrr)
+    map_func <- furrr::future_map
+    map_dbl_func <- furrr::future_map_dbl
+    map_df_func <- furrr::future_map_dfr
+    pmap_df_func <- furrr::future_pmap_dfr
+    plan(multicore)
+  } else {
+    map_func <- purrr::map
+    map_dbl_func <- purrr::map_dbl
+    map_df_func <- purrr::map_df
+    pmap_df_func <- purrr::pmap_df
+  }
+
   if(permutations == 1){
-    map_df(split_vars, function(split_var){
+    map_df_func(split_vars, function(split_var){
       split_var = as.name(split_var)
       dataframe %>%
         group_nest({{split_var}})%>%
-        mutate(n = map_dbl(data, nrow)) %>%
+        mutate(n = map_dbl_func(data, nrow)) %>%
         filter(n > min_sample_size) %>%
         gather(variable, value, -data, -n) %>%
         mutate(value = as.character(value))}) %>%
-      mutate(model = map(data, model)) %>%
+      mutate(model = map_func(data, model)) %>%
       select(data, model, n, everything())
 
   } else if (permutations == 2){
@@ -31,27 +51,37 @@ auto_mm = function(dataframe, model, split_vars, min_sample_size = 30, permutati
       rename(var1 = 1, var2 = 2) %>%
       filter(var1 != var2) %>%
       mutate_all(as.character) %>%
-      pmap_df(function(var1, var2){
+      pmap_df_func(function(var1, var2){
         var1 = as.name(var1)
         var2 = as.name(var2)
         dataframe %>%
           group_nest({{var1}}, {{var2}}) %>%
           gather(variable_1, value_1, -c(2:data)) %>%
           gather(variable_2, value_2, -c(data:value_1)) %>%
-          mutate(n = map_dbl(data, nrow)) %>%
+          mutate(n = map_dbl_func(data, nrow)) %>%
           filter(n > min_sample_size) %>%
-          mutate(model = map(data, model)) %>%
+          mutate(model = map_func(data, model)) %>%
           select(data, model, n, everything())
       })
   } else {warning("Only 1 or 2 permutations are possible.")}
 
-
 }
 
-extract_model_metric = function(dataframe, model_metric){
+extract_model_metric = function(dataframe, model_metric, parallelize = F){
   require(broom)
+
+  if(parallelize == T){
+    require(furrr)
+    plan(multicore)
+    map_func = furrr::future_map
+    map_dbl_func = furrr:::future_map_dbl
+  } else {
+    map_func = purrr::map
+    map_dbl_func = purrr::map_dbl
+  }
+
   dataframe %>%
-    mutate({{model_metric}} := map_dbl(model, function(model){
+    mutate({{model_metric}} := map_dbl_func(model, function(model){
       s = broom::glance(model)
       y = s[[model_metric]]
       return(y)
