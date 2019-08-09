60ec1f9d098450ce6f840445d9331fcb38f5611e
Zanidean <`AZanidean@mhc.ab.ca`>
2019-08-09 13:30:14 -0600

    fix plot

--- R/plot_mm.R
+++ R/plot_mm.R
@@ -10,7 +10,6 @@ plot_mm = function(dataframe, sort){
            value_2 = fct_reorder(value_2, {{sort}}) %>% fct_rev) %>%
     ggplot(aes(x = value_1, y = value_2)) +
     geom_tile(aes(fill = {{sort}}), color = "white") +
-    #geom_text(aes(x = value_1, y = value_2, label = comma(n))) +
     scale_fill_viridis_c() +
     theme_minimal() +
     theme(axis.title = element_blank())
